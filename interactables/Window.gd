55ccaca0cd2d2f2b1d3bf54ced48611493edd1e8
sam effa <sameffa1@gmail.com>
2023-07-05 07:28:44 -0400

    window graphics

--- interactables/Window.gd
+++ interactables/Window.gd
@@ -2,10 +2,11 @@ extends StaticBody2D
 
 
 var REPAIR_SPEED = 1
-export var MAX_BOARD_COUNT = 6
+var MAX_BOARD_COUNT = 6
 var MONEY_FOR_REPAIR = 30
 onready var board_count = MAX_BOARD_COUNT
 var can_be_repaired = true
+var BREAK_ANIMATION_STEP = .1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +17,20 @@ func _process(delta):
 	else:
 		# Dont let the zombies pass through
 		$CollisionShape2D.one_way_collision = false
-
+		
+	# update the board breaking animation manually, make sure it doesnt play
+	# board count 6 = 0
+	# ( MAX (6) - count (6) - 1 ) * step (.1) = 0
+	# ( MAX (6) - count (0) - 1) * step (.1) = .6
+	
+	
 func take_damage(amount):
 	board_count -= 1
 	if board_count < 0:
 		board_count = 0
+	$breakAnimation.play("break")
+	$breakAnimation.seek( (MAX_BOARD_COUNT - board_count - 1) * BREAK_ANIMATION_STEP, true )
+	$breakAnimation.stop(false)
 
 # Repair the window
 func interact(_player):
@@ -39,6 +49,9 @@ func repair_board_animation_finished():
 	can_be_repaired = true
 	if board_count > MAX_BOARD_COUNT:
 		board_count = MAX_BOARD_COUNT
+	$breakAnimation.play("break")
+	$breakAnimation.seek( (MAX_BOARD_COUNT - board_count - 1) * BREAK_ANIMATION_STEP, true )
+	$breakAnimation.stop(false)
 	
 
 
