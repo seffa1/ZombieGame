a1a60519d593d07c8e1228f917e40cea42feecf0
Sam <94399429+seffa1@users.noreply.github.com>
2023-07-06 15:15:08 -0400

    reset highscore button

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const SAVE_DIR = "user://highscore/"
-
+var save_path = SAVE_DIR + "highscore.dat"
 
 func _ready():
 	print('title screen is ready')
@@ -14,36 +14,9 @@ func _ready():
 	# toggle the HUD
 	get_parent().find_node("HUD").visible = false
 	
-	# load high score data
-	var file = File.new()
-	var save_path = SAVE_DIR + "highscore.dat"
-	print("searching save file at" + save_path)
-	if file.file_exists(save_path):
-		var error = file.open(save_path, File.READ)
-		print("title screen high score save file found")
-		
-		if error == OK:
-			var oldSaveData = file.get_var()
-			file.close()
-			print("title screen high score game data loaded")
-			# update title screen high scores
-			$CanvasLayer/highestRoundVal.text = str(oldSaveData["current_level"])
-			$CanvasLayer/mostKillsVal.text = str(oldSaveData["kills"])
-			$CanvasLayer/roundsFiredVal.text = str(oldSaveData["bulletsFired"])
-			$CanvasLayer/roundsHitVal.text = str(oldSaveData["bulletsHit"])
-			var accuracy =  round((float(oldSaveData["bulletsHit"]) / float(oldSaveData["bulletsFired"])) * 100)
-			print("Accuracy" + str(accuracy))
-			$CanvasLayer/AccuracyVal.text = str(accuracy) + ' %'
-			
-			# dataToSave = {
-			# 	"current_level": current_level,
-			#	"bulletsFired": bulletsFired,
-			#	"bulletsHit": bulletsHit,
-			#	"kills": kills
-			#}
-			
+	loadHighScoreData()
 
-## TODO: load the high score data
+
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -69,3 +42,38 @@ func _on_load_pressed():
 			print(game_data)
 		
 	pass # Replace with function body.
+
+func _on_clearScores_pressed():
+	var dir = Directory.new()
+	dir.remove(save_path)
+	print("high scores deleted")
+	loadHighScoreData()
+	
+	
+func loadHighScoreData():
+	# load high score data
+	var file = File.new()
+	
+	print("searching save file at" + save_path)
+	if file.file_exists(save_path):
+		var error = file.open(save_path, File.READ)
+		print("title screen high score save file found")
+		
+		if error == OK:
+			var oldSaveData = file.get_var()
+			file.close()
+			print("title screen high score game data loaded")
+			# update title screen high scores
+			$CanvasLayer/highestRoundVal.text = str(oldSaveData["current_level"])
+			$CanvasLayer/mostKillsVal.text = str(oldSaveData["kills"])
+			$CanvasLayer/roundsFiredVal.text = str(oldSaveData["bulletsFired"])
+			$CanvasLayer/roundsHitVal.text = str(oldSaveData["bulletsHit"])
+			var accuracy =  round((float(oldSaveData["bulletsHit"]) / float(oldSaveData["bulletsFired"])) * 100)
+			print("Accuracy" + str(accuracy))
+			$CanvasLayer/AccuracyVal.text = str(accuracy) + ' %'
+	else:
+		$CanvasLayer/highestRoundVal.text = "0"
+		$CanvasLayer/mostKillsVal.text = "0"
+		$CanvasLayer/roundsFiredVal.text = "0"
+		$CanvasLayer/roundsHitVal.text = "0"
+		$CanvasLayer/AccuracyVal.text = "0"
