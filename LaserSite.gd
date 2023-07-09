d5c541b818550de4cb9d0b9fddbb0a1687979dc6
sam effa <sameffa1@gmail.com>
2023-07-08 20:14:58 -0400

    finished laser sight

--- LaserSite.gd
+++ LaserSite.gd
@@ -19,6 +19,8 @@ onready var fillLine := $FillLine2D
 onready var tweenLine := $TweenLine
 onready var tweenLight := $TweenLight
 onready var light := $Light2D
+onready var collisionParticles := $collisionParticles
+onready var collisionLight := $collisionParticles/Light2D
 
 onready var line_width: float = fillLine.width
 
@@ -26,7 +28,12 @@ var LIGHT_ENERGY = 0.8
 
 func _ready() -> void:
 	set_physics_process(false)
-	fillLine.points[1] = Vector2.ZERO
+	fillLine.points[1] = Vector2.ZERO  # fill line off be default
+	fillLine.width = 0
+	light.energy = 0  # light off be default
+	collisionParticles.emitting = false  # collision particles off be default
+	collisionLight.enabled = false # collision particles light
+	is_casting = false
 
 func set_is_casting(cast: bool) -> void:
 	"""
@@ -39,6 +46,8 @@ func set_is_casting(cast: bool) -> void:
 		fillLine.points[1] = cast_to
 		appear()  # animation of width to go along with the casting
 	else:
+		collisionParticles.emitting = false
+		collisionLight.enabled = false
 		disappear()  # animation of width to go along with the casting
 
 	set_physics_process(is_casting) # what actually gets the laser to start casting
@@ -49,6 +58,7 @@ func _physics_process(delta: float) -> void:
 	cast_to is also set to (0,0) initially and increasing by the cast speed.
 	"""
 	# increase cast to until we hit the max length
+	# will always be pointing to the right. The parent will rotate it.
 	cast_to = (cast_to + Vector2.RIGHT * cast_speed * delta).clamped(max_length)
 	
 	# checks if we collided with something, and makes that the cast point instead
@@ -62,24 +72,27 @@ func cast_beam() -> void:
 	"""
 	var cast_point := cast_to
 
+
 	force_raycast_update()
 
 	if is_colliding():
 		# Our new collision point if we are colliding
 		cast_point = to_local(get_collision_point())
-		print("collisions!")
-		
-		# Set the effect's length to the cast point
-		# $Light2D.global_rotation = get_collision_normal().angle()
-
-		# Set collision particles to the collision point
-		# $collision_particles.position = cast_point
+		# print("collisions!")
+		# print("cast_point: " + str(cast_point)) 
+		collisionParticles.emitting = true
+		collisionLight.enabled = true
+	else:
+		collisionParticles.emitting = false
+		collisionLight.enabled = false
 
 	# with our new cast_point, update the lengths/position of everything
 	fillLine.points[1] = cast_point  # set fill line vector
-	#$Light2D.global_rotation = get_collision_normal().angle() # set light rotation
 	
-	# $collisionParticles.position = cast_point * 0.5
+	# TODO - change length of the light 
+	# light.scale = cast_point.x (or something 
+	
+	collisionParticles.position = cast_point
 	# $collisionParticles.process_material.emission_box_extents.x = cast_point.length() * 0.5
 
 
